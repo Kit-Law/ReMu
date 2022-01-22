9c9f1b919a037fa6e81d33e2d82f264818c1c7de
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-01-22 00:38:04 +0000

    Added pugixml

--- CLI/src/main.cpp
+++ CLI/src/main.cpp
@@ -9,6 +9,8 @@
 
 #include "Tokens/scaleRule.h"
 
+#include "pugixml.hpp"
+
 int main()//const char* argv[]) 
 {
 	std::ifstream stream;
@@ -24,6 +26,19 @@ int main()//const char* argv[])
 	antlr4::tree::ParseTree* tree = parser.script();
 	ReMu::Listener listener;
 	antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+
+	pugi::xml_document doc;
+	pugi::xml_parse_result result = doc.load_file("xgconsole.xml");
+	if (!result)
+		return -1;
+
+	for (pugi::xml_node tool : doc.child("Profile").child("Tools").children("Tool"))
+	{
+		int timeout = tool.attribute("Timeout").as_int();
+
+		if (timeout > 0)
+			std::cout << "Tool " << tool.attribute("Filename").value() << " has timeout " << timeout << "\n";
+	}
 
 	return 0;
 }
