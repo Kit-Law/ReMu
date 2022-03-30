72f7e2d29ec4b2c128c4779ffc0942bbf8c268c5
Kit <34525535+Kit-Law@users.noreply.github.com>
2022-03-30 17:04:49 +0100

    Changed to QT5

---
+++ GUIx86/MainWindow.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QFileDialog>
+
+#include <filesystem>
+#include <fstream>  
+#include <sstream>
+#include <regex>
+
+#include "ui_mainwindow.h"
+
+#include "pugixml.hpp"
+
+#include "SyntaxHighlighter.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    Ui::MainWindow* ui;
+    SyntaxHighlighter* highlighter;
+
+    QTabWidget* tabWidget;
+
+    std::string projectName;
+    std::string projectFile;
+    std::string inputFile;
+    std::string outputFile;
+public:
+    MainWindow(QWidget *parent = Q_NULLPTR);
+    ~MainWindow();
+private slots:
+    void setupEditor();
+
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+
+    void on_actionChange_Input_triggered();
+    void on_actionChange_Output_triggered();
+    void on_actionSave();
+};
+
+void updateProjectDoc(std::string* projectFile, std::string* projectName, std::string* inputFile, std::string* outputFile, const char* project);
+
+inline pugi::xml_document openDoc(const char* input) //TODO: Remove this and use the one in CLI
+{
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(input);
+    if (!result)
+        throw;
+
+    return doc;
+}
+
+inline void closeDoc(pugi::xml_document& doc, const char* output)
+{
+    doc.save_file(output);
+}
